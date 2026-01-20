269d9cdf451d8c94abbb2a1a06f1b55e762a075a
FeeQ06 <awgkunurfikri@gmail.com>
2026-01-20 15:17:00 +0800

    third commit

--- W2/play.php
+++ W2/play.php
@@ -2,4 +2,10 @@
 $name = "Awang";
 echo "<h1>hello World $name</h1>";
 echo 'This is a full stack class';
+
+$name1 = "Awangku Nurfikri Bin Awangku Sophian";
+$name2 = "Miss Athirah Binti Musa";
+echo "<h1>Student Name: $name1</h1>";
+echo "<h2>PA Name: $name2</h2>";
+echo "<h3>Department of Information Technology</h3>";
 ?>
